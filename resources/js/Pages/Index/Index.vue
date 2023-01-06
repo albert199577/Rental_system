2b47668b6f698c9e86e3d5c6d50d6f8d49dfedd5
albert199577 <mso729049@gmail.com>
2023-01-06 14:42:45 +0800

    chore: eslint-vue

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Index</div>
+  <Link href="/hello">
+    Show page
+  </Link>
+  <p>Message: {{ message }}</p>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+defineProps({ message: String })
+</script>
+
+<!-- <script>
+import MainLayout from "../Layouts/MainLayout.vue";
+export default {
+  layout: MainLayout
+}
+</script> -->
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Show</div>
+  <Link href="/">Main page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
+
