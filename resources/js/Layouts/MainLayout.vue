ab991af505634c6e39d8208f89db0d2c31fee86d
albert199577 <mso729049@gmail.com>
2023-01-17 11:30:48 +0800

    style: beauty home page

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,26 @@
 <template>
-  <Link href="/">Main Page</Link>
-  <Link href="/hello">Show Page</Link>
-  <!-- <div>The page with Time {{ timer }}</div> -->
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-  <slot>Default</slot>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('listing.index')">Listings</Link>
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listing.index')">LaraZillow</Link>
+        </div>
+        <div>
+          <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+      <!-- I'm a flash -->
+    </div>
+    <slot>Default</slot>
+  </main>
 </template>
 
 <script setup>
@@ -17,10 +32,6 @@ const flashSuccess = computed(
   () => page.props.value.flash.success,
 )
 
-// import { ref } from 'vue'
-
-// const timer = ref(0)
-// setInterval(() => timer.value++, 1000)
 </script>
 
 <style scoped>
