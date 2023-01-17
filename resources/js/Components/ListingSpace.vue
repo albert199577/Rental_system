e314e3a1a5b7719bdfd34fcd61b711c971143c9b
albert199577 <mso729049@gmail.com>
2023-01-17 15:53:39 +0800

    style: home page UI conponents #3

--- resources/js/Components/ListingSpace.vue
+++ resources/js/Components/ListingSpace.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div>
     <span class="font-bold">{{ listing.beds }}</span> bds <span class="text-gray-400"> | </span>
     <span class="font-bold">{{ listing.baths }}</span> ba <span class="text-gray-400"> | </span>
     <span class="font-bold">{{ listing.area }}</span> m²
